d51b93049b37e66851d2660fb934e2f7119ba163
Aleksey Onishchuk <Onishchuk.aleksey@gmail.com>
2019-04-17 01:26:01 +0300

    test2

---
+++ test_airflow2.py
@@ -0,0 +1,65 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
+
+import logging
+from datetime import datetime
+
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2019, 4, 16),
+}
+
+
+def read_xcoms(**context):
+    for idx, task_id in enumerate(context['data_to_read']):
+        data = context['task_instance'].xcom_pull(task_ids=task_id, key='data')
+        logging.info(f'[{idx}] I have received data: {data} from task {task_id}')
+
+
+def launch_docker_container(**context):
+    # just a mock for now
+    logging.info(context['ti'])
+    logging.info(context['image_name'])
+    my_id = context['my_id']
+    context['task_instance'].xcom_push('data', f'my name is {my_id}', context['execution_date'])
+
+
+with DAG('pipeline_python_2', default_args=default_args) as dag:
+    t1 = BashOperator(
+        task_id='print_date1',
+        bash_command='date')
+
+    t2_1_id = 'do_task_one'
+    t2_1 = PythonOperator(
+        task_id=t2_1_id,
+        provide_context=True,
+        op_kwargs={
+            'image_name': 'task1',
+            'my_id': t2_1_id
+        },
+        python_callable=launch_docker_container
+    )
+
+    t2_2_id = 'do_task_two'
+    t2_2 = PythonOperator(
+        task_id=t2_2_id,
+        provide_context=True,
+        op_kwargs={
+            'image_name': 'task2',
+            'my_id': t2_2_id
+        },
+        python_callable=launch_docker_container
+    )
+
+    t3 = PythonOperator(
+        task_id='read_xcoms',
+        provide_context=True,
+        python_callable=read_xcoms,
+        op_kwargs={
+            'data_to_read': [t2_1_id, t2_2_id]
+        }
+    )
+
+    t1 >> [t2_1, t2_2] >> t3
